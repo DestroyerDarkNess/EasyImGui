9e99c18778e0edd05060eadbf6486b3659c92842
Destroyer <32405118+DestroyerDarkNess@users.noreply.github.com>
2024-11-12 10:14:38 -0400

    firs commit

---
+++ EasyImGui/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyImGui
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
